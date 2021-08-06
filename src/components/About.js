7a694f008d3e6080a2f17644989e10c590c7c0bd
Sim Brody <sim@simbrody.co.uk>
2021-08-06 15:11:03 +0100

    fixed about typos

--- src/components/About.js
+++ src/components/About.js
@@ -23,9 +23,9 @@ const About = () => {
 
             <Typography>I'm an experienced and highly professional front-end developer, specialising in working with health organisations but also available for other select contracts. I've worked as a freelancer with agencies and directly with clients for the past 6 years. Before that I was head of design and technology at an international publishing company and have loads of business experience.</Typography>
             <Typography>
-                I build sites and web apps using the tools that best meet the customer's need. But I often work with React, Redux, Gatsby and the headless CMS approach, where clients get to edit their content using a familiar platform such as WordPress but the front end of the site is delivered via the blazing fast <a href="https://jamstack.org/">JAMStack</a>.
+                I build sites and web apps using the tools that best meet the customer's need. But I like to work with React, Redux, Gatsby and the headless CMS approach, where clients get to edit their content using a familiar platform such as WordPress but the front end of the site is delivered via the super fast <a href="https://jamstack.org/">JAMStack</a>.
             </Typography>
-            <Typography>You can check out some of the additional intereactivity you can get working with technology such as React in <Link to='/demo'>my demo</Link>.</Typography>
+            <Typography>You can check out some of the additional interactivity you can get from technology such as React in <Link to='/demo'>my demo</Link>.</Typography>
             <Typography>I'm also friendly and approachable so do <Link to='/contact'>get in touch</Link> if you have a project we could work together on.</Typography>
         </ThemeContainer>
     )
